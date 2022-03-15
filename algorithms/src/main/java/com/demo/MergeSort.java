0b511409c7c86245f06878b7c7f33aecaaf3eaa7
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-03-16 00:05:49 +0530

    in

---
+++ algorithms/src/main/java/com/demo/MergeSort.java
@@ -0,0 +1,32 @@
+package com.demo;
+
+//Java code to explain double check locking
+public class MergeSort
+{
+	// private instance, so that it can be
+	// accessed by only by getInstance() method
+	private static MergeSort instance;
+
+	private MergeSort()
+	{
+		// private constructor
+	}
+
+	public static MergeSort getInstance()
+	{
+		if (instance == null)
+		{
+			//synchronized block to remove overhead
+			synchronized (MergeSort.class)
+			{
+				if(instance==null)
+				{
+					// if instance is null, initialize
+					instance = new MergeSort();
+				}
+
+			}
+		}
+		return instance;
+	}
+}
