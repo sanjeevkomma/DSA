b9d99fc30cc491faffc2e8aaaba63ac09c1b77ee
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-04-23 11:00:05 +0530

    in

--- data-structures/src/main/java/LinkedList_Find_MiddleElement.java
+++ data-structures/src/main/java/LinkedList_Find_MiddleElement.java
@@ -13,7 +13,75 @@
  *
  * 
  */
-public class LinkedList_MiddleElement {
+
+
+class Node {
+
+	private String data;
+
+	private Node next;
+
+
+	public Node(String data){
+
+		this.data = data;
+	}
+
+	public String getData() {
+
+		return data;
+	}
+
+	public void setData(String data) {
+
+		this.data = data;
+	}
+
+	public Node next() {
+
+		return next;
+	}
+
+	public void setNext(Node next) {
+
+		this.next = next;
+	}
+
+	public String toString(){
+
+		return this.data;
+	}
+}
+
+
+class CustomLinkedList {
+
+	private Node head;
+
+	private Node tail;
+
+	public CustomLinkedList(){
+
+		this.head = new Node("head");
+
+		tail = head;
+	}
+
+	public Node head(){
+
+		return head;
+	}
+
+	public void add(Node node){
+
+		tail.next = node;
+
+		tail = node;
+	}
+
+}
+
+public class LinkedList_Find_MiddleElement {
 
 
 	public static void main(String args[]) {
@@ -53,54 +121,6 @@ public class LinkedList_MiddleElement {
 
 /// 
 
-class LinkedList{
-	private Node head;
-	private Node tail;
 
-	public LinkedList(){
-		this.head = new Node("head");
-		tail = head;
-	}
-
-	public Node head(){
-		return head;
-	}
-
-	public void add(Node node){
-		tail.next = node;
-		tail = node;
-	}
-
-/// 
-	
-	public static class Node{
-		private Node next;
-		private String data;
-
-		public Node(String data){
-			this.data = data;
-		}
-
-		public String data() {
-			return data;
-		}
-
-		public void setData(String data) {
-			this.data = data;
-		}
-
-		public Node next() {
-			return next;
-		}
-
-		public void setNext(Node next) {
-			this.next = next;
-		}
-
-		public String toString(){
-			return this.data;
-		}
-	}
-}
 
 
