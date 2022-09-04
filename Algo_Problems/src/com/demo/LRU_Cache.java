44198df3dffc1a2200d8baf545aeca7800914188
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-09-04 16:27:23 +0530

    in

---
+++ Algo_Problems/src/com/demo/LRU_Cache.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class LRU_Cache {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algo_Problems/src/com/demo/Remove_Element_In_Array.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import java.util.List;
+
+public class Remove_Element_In_Array {
+
+	public static void main(String[] args) {
+		
+		int arr[] = { 1, 2, 3, 4, 5 };
+		
+		arr = removeElment(arr,3);
+		
+		for ( int i = 0 ; i < arr.length ; i ++ ) {
+			
+			System.out.println(arr[i]);
+		}
+	}
+	
+	static int[] removeElment(int arr[],int index) {
+		
+		int result[] = arr;
+		
+		
+		List<Integer> list = null;
+		
+		list.remove(index);
+		
+		return result;
+	}
+
+}
