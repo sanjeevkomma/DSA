7dbbccfb159bb8f8b693f1ea165a4706c7824735
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-09-24 23:23:19 +0530

    in

--- Algo_Problems/src/com/demo/LRU_Cache.java
+++ Algo_Problems/src/com/demo/LRU_Cache.java
@@ -5,24 +5,36 @@ import java.util.Map;
 
 // HashMap , Doubly Linked List to be used
 
+/** 
+ * 
+ * References 
+ * 1. https://www.youtube.com/watch?v=xDEuM5qa0zg
+ * 2. https://www.youtube.com/watch?v=Xc4sICC8m4M
+ *
+ * Time Complexity = O(1) for get & put operations
+ * Space Complexity = 
+ * 
+ * 
+ */
+
 public class LRU_Cache {
-	
+
 	Node head = new Node(0, 0);
 	Node tail = new Node(0,0);
 	Map<Integer,Node> map = new HashMap<Integer, Node>();
 	int capacity; 
-	
-	
+
+
 	public LRU_Cache(int _capacity) {
 		capacity = _capacity;
 		head.next = tail;
 		tail.prev = head; 
 	}
-	
+
 	public int get(int key) {
 		if ( map.containsKey(key) ) {
 			Node node = map.get(key);
-			remove(node);
+			remove(node); // Removing as its Last recently used
 			insert(node);
 			return node.value;
 		}
@@ -30,9 +42,9 @@ public class LRU_Cache {
 			return -1;
 		}
 	}
-	
+
 	public void put(int key, int value) {
-		
+
 		if(map.containsKey(key) ) {
 			remove(map.get(key));
 		}
@@ -41,50 +53,51 @@ public class LRU_Cache {
 		}
 		insert(new Node(key,value));
 	}
-	
+
+	private void remove(Node node) {
+
+		map.remove(node.key);
+		node.prev.next = node.next;
+		node.next.prev = node.prev;
+
+
+	}
+
 	private void insert(Node node) {
-		
+
 		map.put(node.key, node);
 		Node headNext = head.next;
 		head.next = node;
 		node.prev = head;
 		headNext.prev = node;
 		node.next = headNext;
-		
-		
+
+
 	}
 
-	private void remove(Node node) {
-		
-		map.remove(node.key);
-		node.prev.next = node.next;
-		node.next.prev = node.prev;
-		
-		
-	}
-	
+
 	public static void main(String[] args) {
-		
+
 		LRU_Cache cache = new LRU_Cache(3);
-		
+
 		cache.put(1,10);
 		cache.put(2,20);
 		cache.put(3,30);
-		
+
 		cache.get(2);
 		cache.put(4, 40);
 		//cache.put(5, 50);
-		
+
 		System.out.println(cache.map);
-		
+
 
 	}
 
 }
 
-
+// For Doubly Linked List
 class Node {
-	
+
 	Node prev, next;
 	int key , value;
 	Node(int _key,int _value){
