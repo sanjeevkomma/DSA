74afb7ca60b0bc3e2ffc33ec688c5052532e6c7c
Sanjeev Komma <sanjeevkomma@gmail.com>
2024-02-17 18:06:32 +0530

    in

--- src/com/demo/tech/Nearest_Smaller_Numbers.java
+++ src/com/demo/tech/Nearest_Smaller_Numbers.java
@@ -2,9 +2,31 @@ package com.demo.tech;
 
 public class Nearest_Smaller_Numbers {
 
-    public static void main(String args[]){
-
-
-
+    // Prints smaller elements on left side of every element
+    static void printPrevSmaller(int[] arr, int n) {
+        // Always print empty or '_' for first element
+        System.out.print("_, ");
+        // Start from second element
+        for (int i = 1; i < n; i++) {
+            // look for smaller element on left of 'i'
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i]) {
+                    System.out.print(arr[j] + ", ");
+                    break;
+                }
+            }
+            // If there is no smaller element on left of 'i'
+            if (j == -1)
+                System.out.print("_, ");
+        }
     }
+
+    // Driver Code
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 0, 2, 5};
+        int n = arr.length;
+        printPrevSmaller(arr, n);
+    }
+
 }
