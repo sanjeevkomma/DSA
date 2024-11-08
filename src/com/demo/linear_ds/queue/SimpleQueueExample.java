e546c40bde045fb6dd836a7a3dc5f5265ecec3a5
Sanjeev Komma <sanjeevkomma@gmail.com>
2024-11-08 13:01:38 +0530

    Create SimpleQueueExample.java

---
+++ src/com/demo/linear_ds/queue/SimpleQueueExample.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SimpleQueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        
+        // Enqueue elements
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        // Dequeue elements
+        System.out.println("Removed: " + queue.poll());  // Outputs 1
+        System.out.println("Peek: " + queue.peek());     // Outputs 2
+
+        System.out.println("Queue: " + queue);           // Outputs [2, 3]
+    }
+}
