330f1250b8041aa3580b563483129d33125aae3f
Sanjeev Komma <sanjeevkomma@gmail.com>
2024-11-08 13:04:21 +0530

    Create ArrayDequeExample.java

---
+++ src/com/demo/linear_ds/queue/types/ArrayDequeExample.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+// ArrayDeque implements the Deque interface and allows insertion and deletion at both ends.
+
+public class ArrayDequeExample {
+    public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+        
+        // Add elements to both ends
+        deque.addFirst("First");
+        deque.addLast("Last");
+
+        System.out.println("Deque: " + deque);            // Outputs [First, Last]
+
+        // Remove elements from both ends
+        System.out.println("Removed: " + deque.removeFirst()); // Outputs First
+        System.out.println("Removed: " + deque.removeLast());  // Outputs Last
+    }
+}
