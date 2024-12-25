007e4550b22f67076add8faa7c14ef051514a9d3
sanjeev <sanjeevkomma@gmail.com>
2024-12-25 13:17:12 +0530

    in

---
+++ src/com/demo/tech/TopKElements_MinHeap.java
@@ -0,0 +1,59 @@
+package com.demo.tech;
+
+import java.util.PriorityQueue;
+/*
+1. The Top 'K' Elements pattern finds the top k largest or smallest elements in an array
+ or stream of data using heaps or sorting.
+
+Explanation:
+------------
+   1. Min-Heap Setup:
+        i) Use a PriorityQueue to maintain the 𝑘 largest elements encountered so far
+        ii) The heap root (smallest element in the heap) is the k-th largest.
+   2. Add Elements:
+        i) Traverse the array, adding each element to the heap.
+        ii) If the heap size exceeds k, remove the smallest element (poll()).
+   3. Result:
+        i) At the end of traversal, the root of the heap (peek()) is the k-th largest element.
+
+
+Time Complexity:
+----------------
+   * O(Nlogk), where N is the size of the array.
+   * Inserting into the heap takes O(logk), and we process N elements
+
+Space Complexity: O(k) for the heap
+
+ */
+
+
+public class TopKElements_MinHeap {
+
+    public int findKthLargest(int[] nums, int k) {
+        // Min-Heap to store the k largest elements
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(k);
+
+        for (int num : nums) {
+            minHeap.offer(num); // Add current number to the heap
+            // Ensure the heap contains only k elements
+            if (minHeap.size() > k) {
+                minHeap.poll(); // Remove the smallest element
+            }
+        }
+
+        // The root of the heap is the k-th largest element
+        return minHeap.peek();
+    }
+
+    public static void main(String args[]) {
+
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 4;
+
+        TopKElements_MinHeap solution = new TopKElements_MinHeap();
+        int result = solution.findKthLargest(nums, k);
+
+        System.out.println("The " + k + "-th largest element is: " + result);
+        // Output: The 2-th largest element is: 5
+    }
+}
---
+++ src/com/demo/tech/TopKElements_Sorting.java
@@ -0,0 +1,29 @@
+package com.demo.tech;
+
+import java.util.Arrays;
+
+/*
+
+Time Complexity: O(NlogN) for sorting
+Space Complexity: O(1) if sorting is done in-place
+
+ */
+
+public class TopKElements_Sorting {
+
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums); // Sort the array
+        return nums[nums.length - k]; // k-th largest element
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 4;
+
+        TopKElements_Sorting solution = new TopKElements_Sorting();
+        int result = solution.findKthLargest(nums, k);
+
+        System.out.println("The " + k + "-th largest element is: " + result);
+        // Output: The 2-th largest element is: 5
+    }
+}
