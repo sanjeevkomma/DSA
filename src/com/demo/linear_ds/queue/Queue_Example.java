82d75822940852742705ed5c441064185b7901f9
sanjeev <sanjeevkomma@gmail.com>
2025-02-06 00:05:27 +0530

    in

--- src/com/demo/linear_ds/queue/Queue_Example.java
+++ src/com/demo/linear_ds/queue/Queue_Example.java
@@ -5,9 +5,23 @@ import java.util.Queue;
 
 public class Queue_Example {
 
-    Queue queue;
+    public static void main(String args[]){
+        Queue<String> queue = new LinkedList<String>();
 
-    LinkedList linkedList;
+        queue.add("apple");
+        queue.add("banana");
+        queue.add("carrot");
+        queue.add("tomato");
+        queue.add("orange");
 
+        System.out.println(queue.peek()); // Returns the head of the queue
+        System.out.println(queue.element()); // Returns the head of the queue
+
+        System.out.println(queue.poll());  // Removes & Returns the head of the queue
+        System.out.println(queue.remove()); // Removes & Returns the head of the queue
+
+        System.out.println(queue.isEmpty());
+
+    }
 
 }
--- src/com/demo/linear_ds/stack/Stack_Example.java
+++ src/com/demo/linear_ds/stack/Stack_Example.java
@@ -7,20 +7,21 @@ import java.util.Stack;
 // Using Stack Class from Java's java.util Package
 public class Stack_Example {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
+
+        Stack<Integer> stack = new Stack<Integer>();
 
         // Pushing elements
         stack.push(10);
         stack.push(20);
         stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
 
-        // Popping element
-        System.out.println("Popped element: " + stack.pop());
+        System.out.println(stack.pop()); // Removes & Returns the top of the Stack
+        System.out.println(stack.peek()); // Returns the top of the Stack
 
-        // Displaying top element
-        System.out.println("Top element: " + stack.peek());
-
-        System.out.println("Is stack empty? " + stack.isEmpty());
+        System.out.println(stack.isEmpty());
     }
 }
 
