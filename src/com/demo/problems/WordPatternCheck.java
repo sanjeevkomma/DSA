8442625baf3fbfe3eba5ab90fd07c0463ce3a2be
sanjeev <sanjeevkomma@gmail.com>
2025-02-09 21:18:21 +0530

    in

--- src/com/demo/problems/WordPatternCheck.java
+++ src/com/demo/problems/WordPatternCheck.java
@@ -8,36 +8,35 @@ Output : true
 Time Complexity : O(n)
 Space Complexity :
 */
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordPatternCheck {
 
-        public static boolean wordPattern(String pattern, String str) {
-            String[] words = str.split(" ");
-            if (pattern.length() != words.length) {
+    public static boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+            if (!map1.getOrDefault(ch, word).equals(word) || !map2.getOrDefault(word, ch).equals(ch)) {
                 return false;
             }
-
-            Map<Object, Integer> map = new HashMap<Object, Integer>();
-
-            for (int i = 0; i < words.length; i++) {
-                char ch = pattern.charAt(i);
-                String word = words[i];
-                // Check if both character and word have the same last index
-                Integer charIndex = map.put(ch, i);
-                Integer wordIndex = map.put(word, i);
-                if (charIndex != null && !charIndex.equals(wordIndex)) {
-                    return false;
-                }
-            }
-            return true;
+            map1.put(ch, word);
+            map2.put(word, ch);
         }
+        return true;
+    }
 
-        public static void main(String[] args) {
-            System.out.println(wordPattern("abba", "dog cat cat dog")); // true
-            System.out.println(wordPattern("abba", "dog cat cat fish")); // false
-            System.out.println(wordPattern("aaaa", "dog cat cat dog")); // false
-            System.out.println(wordPattern("abba", "dog dog dog dog")); // false
-        }
+    public static void main(String[] args) {
+        System.out.println(wordPattern("abba", "dog cat cat dog")); // true
+        System.out.println(wordPattern("abba", "dog cat cat fish")); // false
+        System.out.println(wordPattern("aaaa", "dog cat cat dog")); // false
+        System.out.println(wordPattern("abba", "dog dog dog dog")); // false
+    }
 }
