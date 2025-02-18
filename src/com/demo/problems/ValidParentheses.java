fad46d8511a15b1c66e81e352e16f8863604db0d
sanjeev <sanjeevkomma@gmail.com>
2025-02-18 18:21:13 +0530

    in

--- src/com/demo/problems/ValidParentheses.java
+++ src/com/demo/problems/ValidParentheses.java
@@ -6,27 +6,27 @@ public class ValidParentheses {
 
     public static void main(String args[]) {
 
-        String str1 = "([([([])]))"; // "([]{})";
+        String str1 = "([([([])])])"; // "([]{})";
 
         System.out.println(isValid(str1));
 
     }
 
-    private static boolean isValid(String s) {
+    private static boolean isValid(String str) {
         // Stack to store left symbols
-        Stack<Character> leftSymbols = new Stack<>();
+        Stack<Character> leftSymbols = new Stack<Character>();
         // Loop for each character of the string
-        for (char c : s.toCharArray()) {
+        for (char ch : str.toCharArray()) {
             // If left symbol is encountered
-            if (c == '(' || c == '{' || c == '[') {
-                leftSymbols.push(c);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                leftSymbols.push(ch);
             }
             // If right symbol is encountered
-            else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+            else if (ch == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
                 leftSymbols.pop();
-            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+            } else if (ch == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
                 leftSymbols.pop();
-            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+            } else if (ch == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
                 leftSymbols.pop();
             }
             // If none of the valid symbols is encountered
