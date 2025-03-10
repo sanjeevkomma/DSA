634265bd86f762be1c2883fec16d0d4f90bef47b
sanjeev <sanjeevkomma@gmail.com>
2025-03-10 21:05:30 +0530

    in

--- src/com/demo/problems/Merge_Sorted_Arrays.java
+++ src/com/demo/problems/Merge_Sorted_Arrays.java
@@ -13,7 +13,7 @@ Space Complexity : O(m + n)
 */
 public class Merge_Sorted_Arrays {
 
-    public static int[] mergeSortedArrays(int[] a, int[] b) {
+    public static int[] mergeSortedArraysKeepDuplicates(int[] a, int[] b) {
         int m = a.length, n = b.length;
         int[] result = new int[m + n];
         int i = 0, j = 0, k = 0;
@@ -40,10 +40,52 @@ public class Merge_Sorted_Arrays {
         return result;
     }
 
+    public static int[] mergeSortedArraysRemoveDuplicates(int[] a, int[] b) {
+        int m = a.length, n = b.length;
+        int result[] = new int[m + n]; // Temporary array
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (a[i] < b[j]) {
+                if (k == 0 || result[k - 1] != a[i]) {
+                    result[k++] = a[i];
+                }
+                i++;
+            } else if (b[j] < a[i]) {
+                if (k == 0 || result[k - 1] != b[j]) {
+                    result[k++] = b[j];
+                }
+                j++;
+            } else { // a[i] == b[j], add only one copy
+                if (k == 0 || result[k - 1] != a[i]) {
+                    result[k++] = a[i];
+                }
+                i++;
+                j++;
+            }
+        }
+
+        while (i < m) { // Remaining elements in a[]
+            if (k == 0 || result[k - 1] != a[i]) {
+                result[k++] = a[i];
+            }
+            i++;
+        }
+
+        while (j < n) { // Remaining elements in b[]
+            if (k == 0 || result[k - 1] != b[j]) {
+                result[k++] = b[j];
+            }
+            j++;
+        }
+
+        return Arrays.copyOf(result, k); // Resize to final size
+    }
+
     public static void main(String[] args) {
         int[] a = {4, 8, 12};
-        int[] b = {1, 3, 56, 78, 99, 100};
-        int[] mergedArray = mergeSortedArrays(a, b);
+        int[] b = {1, 3, 56, 56, 78, 99, 100};
+        int[] mergedArray = mergeSortedArraysKeepDuplicates(a, b);
         System.out.println("Merged Sorted Array: " + Arrays.toString(mergedArray));
     }
 }
