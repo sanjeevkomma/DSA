d6e22f150918da97379ddea2eed49c1f7dbe0580
sanjeev <sanjeevkomma@gmail.com>
2025-03-30 22:06:27 +0530

    in

--- src/com/demo/linear_ds/queue/PriorityQueue_Example.java
+++ src/com/demo/linear_ds/queue/PriorityQueue_Example.java
@@ -1,23 +1,27 @@
 package com.demo.linear_ds.queue;
 
 import java.util.PriorityQueue;
+import java.util.Queue;
 
-// PriorityQueue as a Priority Queue
-// PriorityQueue provides a priority-based queue where elements are ordered according
-// to their natural ordering or by a custom comparator.
+/* PriorityQueue as a Priority Queue
+ * PriorityQueue provides a priority-based queue where elements are ordered according
+to their natural ordering or by a custom comparator
+ * Elements are sorted in ascending order in Prority Queue
+ */
+
 
 public class PriorityQueue_Example {
     public static void main(String[] args) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
 
         // Enqueue elements
         priorityQueue.add(10);
-        priorityQueue.add(5);
-        priorityQueue.add(20);
+        priorityQueue.add(5777);
+        priorityQueue.add(2000);
 
         // Elements are dequeued based on priority (natural ordering in this case)
-        System.out.println("Removed: " + priorityQueue.poll());  // Outputs 5
-        System.out.println("Peek: " + priorityQueue.peek());      // Outputs 10
+        System.out.println("Removed: " + priorityQueue.poll());  // Outputs 10
+        System.out.println("Peek: " + priorityQueue.peek());      // Outputs 2000
     }
 }
 
--- src/com/demo/linear_ds/queue/Queue_Example.java
+++ src/com/demo/linear_ds/queue/Queue_Example.java
@@ -20,12 +20,13 @@ public class Queue_Example {
 
         System.out.println("peek : " + queue.peek()); // Returns the head of the Queue(FIFO)
         System.out.println("poll : " + queue.poll()); // Removes & Returns the head of the Queue(FIFO)
-        System.out.println("remove1 : " + queue.remove());
+        System.out.println("remove1 : " + queue.remove()); // Remove the head of the Queue
         System.out.println("remove2 : " + queue.remove("Popagranate"));
-        System.out.println("front element : " + queue.element());
+        System.out.println("front element : " + queue.element()); // Returns the head of the Queue
         System.out.println("isEmpty : " + queue.isEmpty());
         System.out.println("contains : " + queue.contains("Carrot"));
         System.out.println("size : " + queue.size());
+        System.out.println("toArray : " + queue.toArray()); // convert Queue to Array
         System.out.println("toArray element : " + queue.toArray()[1]);
 
         System.out.println("Queue2: " + queue);
