32a53e90ad8db6b8a78c576a6ad954645a000e33
sanjeev <sanjeevkomma@gmail.com>
2025-05-16 22:54:07 +0530

    in

--- src/com/demo/core/algos/sorting_algos/QuickSort_Recursion_Swapping.java
+++ src/com/demo/core/algos/sorting_algos/QuickSort_Recursion_Swapping.java
@@ -25,67 +25,47 @@ public class QuickSort_Recursion_Swapping {
 
 	public static void main(String[] args) {
 
-		int arr[] = { 222, 0, -11, 30, 24, -7, 31, 16, 39 };  
-
-		quickSort(arr, 0, arr.length - 1);  
-
-		System.out.println("After sorting array elements are - ");  
-
+		int arr[] = { 222, 0, -11, 30, 24, -7, 31, 16, 39 };
+		quickSort(arr, 0, arr.length - 1);
+		System.out.println("After sorting array elements are - ");
 		printArray(arr);
 	}
 
 
 	static void quickSort(int arr[], int beginIndex, int endIndex) {
 
-		if ( beginIndex < endIndex ) { 
-
+		if ( beginIndex < endIndex ) {
 			int pivotIndex = partition(arr, beginIndex, endIndex);
-
 			quickSort(arr, beginIndex, pivotIndex - 1);
-
 			quickSort(arr, pivotIndex + 1, endIndex);
-
 		}
-
 	}
 
 	static int partition(int arr[], int beginIndex, int endIndex){
 
 		int pivot = arr[endIndex];
-		
 		int pivotIndex = beginIndex - 1 ;
 
 		for( int i = beginIndex; i <= endIndex - 1; i ++ ){
-
 			if ( arr[i] < pivot ){
-
 				pivotIndex ++;
-
 				swap(arr, pivotIndex, i);
 			}
 		}
 
 		swap(arr, pivotIndex + 1, endIndex);
-		
 		return pivotIndex + 1 ;
 
 	}
 
 	static void swap(int[] arr, int i, int j){
-
 		int temp = arr[i];
-
 		arr[i] = arr[j];
-
 		arr[j] = temp;
-
 	}
 
-	static void printArray(int arr[] )  {  
-
-		for  ( int i = 0; i < arr.length; i ++ )  
-
+	static void printArray(int arr[] )  {
+		for  ( int i = 0; i < arr.length; i ++ )
 			System.out.print(arr[i] + " ");  
-	}  
-
+	}
 }
