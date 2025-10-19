9be1402e8f65e9623cba995dc11002defc8566da
sanjeev <sanjeevkomma@gmail.com>
2025-10-19 14:44:34 +0530

    in

--- src/com/demo/core/algos/leetcode/Blind_75/README.md
+++ src/com/demo/core/algos/leetcode/Blind_75/README.md
@@ -1,1 +1,13 @@
 * [Blind 75](https://leetcode.com/problems/longest-consecutive-sequence/description/?envType=problem-list-v2&envId=oizxjoit&)
+
+/*
+Input:
+Output:
+Problem :
+Approach :
+Strategy :
+Pattern :
+Explanation:
+Time Complexity:
+Space Complexity:
+*/
--- src/com/demo/core/algos/leetcode/Blind_75/TwoSum.java
+++ src/com/demo/core/algos/leetcode/Blind_75/TwoSum.java
@@ -1,4 +1,15 @@
 package com.demo.core.algos.leetcode.Blind_75;
 
+/*
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Problem :
+Approach :
+Strategy :
+Pattern :
+Explanation : nums[0] + nums[1] == 9, we return [0, 1].
+Time Complexity :
+Space Complexity :
+ */
 public class TwoSum {
 }
